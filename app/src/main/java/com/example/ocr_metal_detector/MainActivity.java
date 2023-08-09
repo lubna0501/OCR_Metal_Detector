441c60edeb4a02a1436da4bcbef0eb32a5984573
lubna0501 <lubnaaggarwal2001@gmail.com>
2023-08-09 15:29:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ocr_metal_detector/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.ocr_metal_detector;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+import java.io.ByteArrayOutputStream;
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends AppCompatActivity {
+ImageView image;
+TextView textresult;
+ Button button;
+
+    private static final int REQUEST_IMAGE_GALLERY = 1;
+    private static final int REQUEST_IMAGE_CAMERA = 2;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image= findViewById(R.id.imageselector);
+        button= findViewById(R.id.buttonselector);
+        textresult= findViewById(R.id.textresult);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImagePickerDialog();
+            }
+        });
+
+    }
+
+    private void showImagePickerDialog() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Intent chooserIntent = Intent.createChooser(galleryIntent, "Select Image");
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{cameraIntent});
+        startActivityForResult(chooserIntent, REQUEST_IMAGE_GALLERY);
+        startActivityForResult(chooserIntent,REQUEST_IMAGE_CAMERA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_GALLERY) {
+                Uri selectedImageUri = data.getData();
+                if (selectedImageUri != null) {
+                    image.setImageURI(selectedImageUri);
+                    performTextRecognition(selectedImageUri);
+                }
+            }
+            else if (requestCode == REQUEST_IMAGE_CAMERA)
+            {
+                if (data != null)
+                {
+                    Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+                    image.setImageBitmap(imageBitmap);
+                    Uri imageUri = getImageUri(imageBitmap);
+                    performTextRecognition(imageUri);
+                }
+            }
+        }
+    }
+
+    private Uri getImageUri(Bitmap bitmap)
+    {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "Title", null);
+        return Uri.parse(path);
+    }
+
+    private void performTextRecognition(Uri imageUri)
+    {
+        try {
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+            InputImage inputImage = InputImage.fromBitmap(bitmap, 0);
+            TextRecognizer textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+            Task<Text> task = textRecognizer.process(inputImage);
+            task.addOnSuccessListener(new OnSuccessListener<Text>() {
+                @Override
+                public void onSuccess(Text text) {
+                    String recognizedText = text.getText();
+                    textresult.setText(recognizedText);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "..Text recognition failed..", Toast.LENGTH_LONG).show();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Text__recognition__failed", Toast.LENGTH_LONG).show();
+        }
+    }
+}
